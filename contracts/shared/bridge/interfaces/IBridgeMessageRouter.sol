03c5dccef226d23ef1fbad73370eb0a8d93f9873
scryptex <waitlisttt@gmail.com>
2025-06-14 02:24:39 +0000

    f

--- contracts/shared/bridge/interfaces/IBridgeMessageRouter.sol
+++
@@ -1,8 +0,0 @@
-
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-
-interface IBridgeMessageRouter {
-    function sendMessage(uint256 dstChainId, bytes calldata payload) external payable;
-    function receiveMessage(uint256 srcChainId, bytes calldata payload) external;
-}
--- contracts/shared/bridge/interfaces/IFeeTreasury.sol
+++
@@ -1,9 +0,0 @@
-
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-
-interface IFeeTreasury {
-    function recordFee(address token, uint256 amount) external;
-    function withdrawFees(address token, uint256 amount) external;
-    function getAvailableFees(address token) external view returns (uint256);
-}
